0b9c71684e3ebae7851027ed062bb8a3251864a6
Benjamin Gerdjunis <benjamingerdjunis@gmail.com>
2025-01-08 13:42:52 -0500

    Added winter theming and corrected effect size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def seasonal_background
+    month = Time.current.month
+
+    if month.between?(9, 11)
+      "background-image: url(#{asset_path('background.png')});"
+    else
+      "background-image: url(#{asset_path('background_winter.png')});"
+    end
+  end
 end
