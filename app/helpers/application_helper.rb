98ec1093cc27f1012a56773d487699c281fb7c68
Benjamin Gerdjunis <benjamingerdjunis@gmail.com>
2025-01-09 22:43:37 -0500

    Font set

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def seasonal_background
     month = Time.current.month
 
-    if month.between?(9, 11)
+    if month.between?(9, 12)
       "background-image: url(#{asset_path('background.png')});"
-    else
+    elsif month.between?(1, 3)
       "background-image: url(#{asset_path('background_winter.png')});"
     end
   end
